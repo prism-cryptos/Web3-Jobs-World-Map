6192972fa7068856ebf0f656e47b78fad465daea
prism <prismeconomy.contact@gmail.com>
2022-08-23 22:50:04 +0900

    modify 230822

--- get_the_number_of_web3_jobs_each_country.py
+++ get_the_number_of_web3_jobs_each_country.py
@@ -1,29 +1,25 @@
 import time
+from datetime import datetime, timezone
 import warnings
 import os
-import sys
 import re
-import codecs
-import requests
-import json
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys  ##Importing Keys is essential to work this script
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import pandas as pd
 import numpy as np
-import pycountry
 from datetime import date
-from gspread_dataframe import set_with_dataframe
+import pycountry
 
 import gspread
+from gspread_dataframe import set_with_dataframe
 from oauth2client.service_account import ServiceAccountCredentials 
 
 
 options = webdriver.ChromeOptions()
 
 warnings.simplefilter("ignore")
-options.add_argument('--headless')
+#options.add_argument('--headless')
 options.add_argument('--disable-extensions')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
@@ -34,62 +30,92 @@ options.page_load_strategy = 'eager' #https://www.selenium.dev/ja/documentation/
 
 #####each_country
 def each_country():
-    global all_country_data
 
-    driver = webdriver.Chrome('./chromedriver.exe', options=options)
-    driver.implicitly_wait(10)
+    global driver
+
+    driver = webdriver.Chrome(os.getcwd() + './chromedriver.exe', options=options)
+    driver.implicitly_wait(5)
     driver.get("https://cryptocurrencyjobs.co/")
-    #time.sleep(2)
 
     while len(driver.find_elements_by_css_selector('ol.ais-Hits-list > li.ais-Hits-item')) == 0:
         print("reload")
         driver.refresh()
-        #time.sleep(2)
     else:
+        loop_process()
+        driver.quit()
 
-        the_number_list = list()
+def loop_process():
 
-        for i in range(0, len(pycountry.countries)):
-            
-            country_name = all_country_data[i,1]
-            
-            country_search_box = driver.find_element_by_xpath('//div[@id="aa-search-input"]/div/div/span/input')
-            time.sleep(3) ##This buffer time is essential.
-            country_search_box.send_keys(str(country_name))
-            print(country_name)
-            time.sleep(3) ##This buffer time is essential.
-            
-            search_country = driver.find_elements_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-2"]/span/div') #caution: "elements"
-            
-            if len(search_country) > 0:  ##https://qiita.com/captainUmaru/items/1d9c1c5e37da986404f1
-                search_each_country = driver.find_element_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-2"]/span/div') #caution: "element"
-                time.sleep(5)
-                search_each_country.click()
-                time.sleep(5)  ##This buffer time is essential.
+    global all_country_data
+    global the_number_in_each_country
+    the_number_in_each_country = []
+
+    for i in range(0, len(pycountry.countries)):
+        
+        country_name = all_country_data[i,1]
+        country_search_box = driver.find_element_by_xpath('//div[@id="aa-search-input"]/div/div/span/input')
+        country_search_box.send_keys(str(country_name))
+        time.sleep(1.5)
+
+        count = len(driver.find_elements_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-2"]/span/div')) #to check the existence of the element
+        
+        if count == 0:
+            the_number_in_each_country.append(None)
+            print(the_number_in_each_country)
+
+            country_search_box.send_keys(Keys.CONTROL + "a")
+            country_search_box.send_keys(Keys.DELETE)
+
+            print("count 0")
+
+        else:
+            while count != 1:
+                count = len(driver.find_elements_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-2"]/span/div'))
                 
-                text = driver.find_element_by_xpath('//div[@id="stats"]/div/span').text
-                the_data = re.sub(r"\D", "", text)
-                the_number_list.append(int(the_data))
-                print(the_number_list)
+                #correction processing(if the variable "count" become more than 1 temporally bacause of bad internet connection, this process can adjust them.)
+                if count == 0:
+                    the_number_in_each_country.append(None)
+                    country_search_box.send_keys(Keys.CONTROL + "a")
+                    country_search_box.send_keys(Keys.DELETE)
+                    break
+                else:
+                    pass
 
-                country_search_box.send_keys(Keys.CONTROL + "a")
-                country_search_box.send_keys(Keys.DELETE)
             else:
-                the_number_list.append(int(0))
-                print(the_number_list)
+                print("count 1")
+                search_country = driver.find_element_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-2"]/span/div')
 
-                country_search_box.send_keys(Keys.CONTROL + "a")
-                country_search_box.send_keys(Keys.DELETE)
+                #checking the correspondence between the country name got and the target country
+                while search_country.text == str(search_list[i]):  ##https://qiita.com/captainUmaru/items/1d9c1c5e37da986404f1
+                    search_each_country = driver.find_element_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-2"]/span/div')
+                    search_each_country.click()
+                    time.sleep(1)
 
-    from_list_to_array = np.array([the_number_list])
-    print(all_country_data)
+                    #judging whether loading is done or not
+                    while str(driver.find_element_by_xpath('//div[@id="searchbox"]/div/form/span').get_attribute('hidden')) != 'true':
+                        time.sleep(0.1)
+                        print('wait loading...')
+
+                    else:
+                        text = driver.find_element_by_xpath('//div[@id="stats"]/div/span').text
+                        the_data = re.sub(r"\D", "", text)
+                        the_number_in_each_country.append(int(the_data))
+                        print(the_number_list)
+
+                        country_search_box.send_keys(Keys.CONTROL + "a")
+                        country_search_box.send_keys(Keys.DELETE)
+                else:
+                    #If there is no correspondence, proceed next country. 
+                    the_number_in_each_country.append(None)
+                    country_search_box.send_keys(Keys.CONTROL + "a")
+                    country_search_box.send_keys(Keys.DELETE)
+                    break
+
+    from_list_to_array = np.array([the_number_in_each_country])
     all_country_data = np.insert(all_country_data, 2, from_list_to_array, axis=1)
     print(all_country_data)
 
-    driver.quit()
-
 def get_country_code_and_name_list():
-    ###COMPLETED
     global all_country_data
 
     for i in range(0, len(pycountry.countries)-1):
@@ -113,15 +139,16 @@ def each_country_to_google_sheet():
     #set values in "each country" spreadsheet
     code_name_number_df = pd.DataFrame(all_country_data).transpose()
 
-    str_list = list(filter(None, each_country_ws.col_values(2))) #set 2(column B) to get the row that data is filled in it. 
+    str_list = list(filter(None, each_country_ws.col_values(1))) #set 1(column A) to get the row that data is filled in it. 
     Last_row = len(str_list)+1
 
-    today = date.today()
+    today = datetime.now(timezone.utc)
     today = today.strftime("%d/%m/%Y")
 
     each_country_ws.update_cell(Last_row, 1, today)
     set_with_dataframe(each_country_ws, pd.DataFrame(code_name_number_df[2:3]),  row = Last_row, col = 2, include_index = False, include_column_header = False)
 
+    
 def scrape_each_country():
     ##### get and write data of each_country 
     global all_country_data
--- get_the_number_of_web3_jobs_total_remote_region.py
+++ get_the_number_of_web3_jobs_total_remote_region.py
@@ -1,24 +1,18 @@
 import time
+from datetime import datetime, timezone
 import warnings
 import os
-import sys
 import re
-import codecs
-import requests
-import json
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys  ##Importing Keys is essential to work this script
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import pandas as pd
 import numpy as np
-import pycountry
 from datetime import date
-from gspread_dataframe import set_with_dataframe
 
 import gspread
+from gspread_dataframe import set_with_dataframe
 from oauth2client.service_account import ServiceAccountCredentials 
-
 
 options = webdriver.ChromeOptions()
 
@@ -37,41 +31,68 @@ options.page_load_strategy = 'eager' #https://www.selenium.dev/ja/documentation/
 #####total_remote_region
 def total_remote_region():
 
+    global driver
     global total_remote_region_list
 
-    driver = webdriver.Chrome(os.getcwd() + './chromedriver.exe', options=options)
+    driver = webdriver.Chrome(os.getcwd() + './chromedriver.exe', options = options)
     driver.implicitly_wait(10)
     driver.get("https://cryptocurrencyjobs.co/")
-    time.sleep(2)
 
     while len(driver.find_elements_by_css_selector('ol.ais-Hits-list > li.ais-Hits-item')) == 0:
         print("reload")
         driver.refresh()
-        time.sleep(2)
     else:
+        #get the number of total jobs
         total_number = driver.find_element_by_xpath('//div[@id="stats"]/div/span').text  ##caution :write element. If I write elements, the error "'list' object has no attribute 'text'" appears.
         total_number = re.sub(r"\D", "", total_number)
         total_remote_region_list.append(total_number)
-        
-        search_list = ["Remote", "Asia", "Europe", "Africa", "US" , "Canada", "Latin America"]
-        country_search_box = driver.find_element_by_xpath('//div[@id="aa-search-input"]/div/div/span/input')
-        time.sleep(2)
 
-        for i in range(0, len(search_list)):
-            country_search_box.send_keys(str(search_list[i]))
-            time.sleep(5)  ##This buffer time is essential.
+        #get the number of remote and each region
+        loop_process(total_remote_region_list)
+        driver.quit()    
+
+def loop_process(total_remote_region_list):
+
+    search_list = ["Remote", "Asia", "Europe", "Africa", "US" , "Canada", "Latin America"]
+    country_search_box = driver.find_element_by_xpath('//div[@id="aa-search-input"]/div/div/span/input')
+
+    for i in range(0, len(search_list)):
+        country_search_box.send_keys(str(search_list[i]))
+        count = len(driver.find_elements_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-3"]/span/div'))
+
+        #if only one element exists, continue the process(below else:)
+        while count != 1:
+            count = len(driver.find_elements_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-3"]/span/div'))
+
+        else:
             search_content = driver.find_element_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-3"]/span/div') #caution: "element"
-            search_content.click()
-            time.sleep(5)  ##This buffer time is essential.
-            text = driver.find_element_by_xpath('//div[@id="stats"]/div/span').text
-            the_data = re.sub(r"\D", "", text)
-            print(the_data)
-            total_remote_region_list.append(the_data)
+            
+            #checking the correspondence between the country name got and the target country name
+            while search_content.text == str(search_list[i]):
+                search_content.click()
+                time.sleep(1)
 
-            country_search_box.send_keys(Keys.CONTROL + "a")
-            country_search_box.send_keys(Keys.DELETE)
+                #judging whether loading is done or not
+                while str(driver.find_element_by_xpath('//div[@id="searchbox"]/div/form/span').get_attribute('hidden')) != 'true':
+                    print('wait loading...')
+                    time.sleep(1)
+                else:
+                    text = driver.find_element_by_xpath('//div[@id="stats"]/div/span').text
+                    the_data = re.sub(r"\D", "", text)
+                    print(the_data)
+                    total_remote_region_list.append(the_data)
 
-        driver.quit()
+                    country_search_box.send_keys(Keys.CONTROL + "a")
+                    country_search_box.send_keys(Keys.DELETE)
+
+                    break
+
+                break
+                
+            else:
+               search_content = driver.find_element_by_xpath('//div[@id="aa-search-input"]/div/div/span/span/div[@class="aa-dataset-3"]/span/div') #caution: "element"
+               print("error")
+
 
 def total_remote_region_to_google_sheet():
 
@@ -83,11 +104,12 @@ def total_remote_region_to_google_sheet():
     SPREADSHEET_KEY = '1QFrs12Rkdi1dMHX8Wki1f3BsPM4MY2mT1v_ro21TjOI'
     total_remote_region_ws = gc.open_by_key(SPREADSHEET_KEY).worksheet('Total, Remote, Region')
 
-    #set values in "remote & Total" spreadsheet
+    #set values in "Total, Remote, Region" spreadsheet
     str_list = list(filter(None, total_remote_region_ws.col_values(2))) #set 2(column B) to get the row that data is filled in it. 
     Last_row = len(str_list)+1
 
-    today = date.today()
+    #today = date.today()
+    today = datetime.now(timezone.utc)
     today = today.strftime("%d/%m/%Y")
 
     total_remote_region_ws.update_cell(Last_row, 1, today)  ##caution: gspread accepts row and column values that start at 1, not 0.
@@ -98,6 +120,7 @@ def total_remote_region_to_google_sheet():
     total_remote_region_ws.update_cell(Last_row, 6, int(total_remote_region_list[4])) #Africa
     total_remote_region_ws.update_cell(Last_row, 7, int(total_remote_region_list[5]) + int(total_remote_region_list[6])) #North America
     total_remote_region_ws.update_cell(Last_row, 8, int(total_remote_region_list[7])) #Latin America
+
 
 #####RUN
 def scrape_total_remote_region():
